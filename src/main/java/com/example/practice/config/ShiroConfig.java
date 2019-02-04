53b15dcd9e7ebc48b44fb701220fabe77e84e3a0
CloseSu <wei33107@gmail.com>
2019-02-04 18:47:29 +0800

    add redis to shiro

--- src/main/java/com/example/practice/config/ShiroConfig.java
+++ src/main/java/com/example/practice/config/ShiroConfig.java
@@ -2,10 +2,15 @@ package com.example.practice.config;
 
 import com.example.practice.shrio.CustomRealm;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +25,8 @@ public class ShiroConfig {
     public DefaultWebSecurityManager defaultWebSecurityManager(CustomRealm customRealm) {
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
         securityManager.setRealm(customRealm);
+        securityManager.setRememberMeManager(rememberMeManager());
+        securityManager.setCacheManager(cacheManager());
         return securityManager;
     }
 
@@ -33,4 +40,25 @@ public class ShiroConfig {
         return shiroFilterFactoryBean;
     }
 
+
+    public SimpleCookie rememberMeCookie() {
+        SimpleCookie cookie = new SimpleCookie("rememberMe");
+        cookie.setMaxAge(86400);
+        return cookie;
+    }
+
+    public CookieRememberMeManager rememberMeManager() {
+        log.info("----------------------rememberMeManager----------------------------");
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+        cookieRememberMeManager.setCipherKey(Base64.decode("4AvVhmFLUs0KTA3Kprsdag=="));
+        return cookieRememberMeManager;
+    }
+
+    public RedisCacheManager cacheManager() {
+        log.info("----------------------cacheManager----------------------------");
+        RedisCacheManager redisCacheManager = new RedisCacheManager();
+        redisCacheManager.setRedisManager(new RedisManager());
+        return redisCacheManager;
+    }
 }
